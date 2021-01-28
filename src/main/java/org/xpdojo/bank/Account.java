bc94d871886b66f96b4e297e08ca6a919cea2ee4
drastevens <andrewst@bogus.net>
2021-01-28 15:20:14 +0000

    added deposit

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,7 +1,13 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance = 0;
+
     public int balance() {
-        return 1;
+        return 0;
+    }
+
+    public void depositFunds(int amount) {
+        balance += amount;
     }
 }
